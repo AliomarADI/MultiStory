63bedb3e70cea8c1438b8fc4d116664ae43fbb23
Echo <aliomaramerica@gmail.com>
2020-10-09 19:32:28 +0600

    9.10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {MainComponent} from './main/main.component';
+import {ProfilePageComponent} from './profile-page/profile-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {InputOutputComponent} from './input-output/input-output.component';
+import {MyIdeasComponent} from './my-ideas/my-ideas.component';
+import {CreateHeroComponent} from './create-hero/create-hero.component';
+import {CreateWorldComponent} from './create-world/create-world.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'profile', component: ProfilePageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'createHero', component: CreateHeroComponent },
+  {path: 'createWorld', component: CreateWorldComponent },
+  {path: 'myIdeas', component: MyIdeasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { CarouselStoriesComponent } from './carousel-stories/carousel-stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {RouterModule} from '@angular/router';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { InputOutputComponent } from './input-output/input-output.component';
+import { ListLinksProfileComponent } from './list-links-profile/list-links-profile.component';
+import { CreateWorldComponent } from './create-world/create-world.component';
+import { CreateHeroComponent } from './create-hero/create-hero.component';
+import { MyIdeasComponent } from './my-ideas/my-ideas.component';
+import {FormsModule} from '@angular/forms';
+
+const routes = [
+  {path: 'login', component: LoginPageComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    CarouselStoriesComponent,
+    FooterComponent,
+    LoginPageComponent,
+    ProfilePageComponent,
+    RegisterPageComponent,
+    InputOutputComponent,
+    ListLinksProfileComponent,
+    CreateWorldComponent,
+    CreateHeroComponent,
+    MyIdeasComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(routes),
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
